4687dee8942a4fc1d8a299440184dab3ae6ad44e
shlomi <valdmanshlomi@gmail.com>
2026-03-27 19:14:42 +0300

    Add first GitHub Actions workflow

--- server.py
+++ server.py
@@ -6,13 +6,4 @@ app = Flask(__name__)
 def home():
     return "Hello from my first server!"
 
-app.run(port=5000)
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello from my first server!"
-
-app.run(port=5000)
+app.run(host="0.0.0.0", port=5000)
